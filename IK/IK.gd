d9cc7b5cd2eecfe0dae5366ad70026ef7e2d0789
PRLHA <01171903@pw.edu.pl>
2022-12-21 00:03:09 +0100

    IK (zintegrowane 2)
    Dodałem scenę IK do sceny "HandManipulator.tscn" oraz zmieniłem parę rzeczy w skrypcie IK.tscn (is enabled: optymalizacja na przyszłość - wyłączać IK kiedy nie w użyciu)
    Bugfixy w skrypcie IK (zmiany w linijce 35 i 43) od teraz początkiem łańcucha IK jest global_position a nie pozycja 0, 0

--- IK/IK.gd
+++ IK/IK.gd
@@ -10,6 +10,11 @@ var test_array = []
 var mouseposition
 var angle
 
+export (float) var joint_length = 20
+export (NodePath) var arms_target = "."
+var is_active = true
+var target_ref = self
+
 
 #Calculating the base of the move vector
 func calculate_normal(base, target):
@@ -19,6 +24,7 @@ func calculate_normal(base, target):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# loading joints into the array
+	target_ref = get_node(arms_target)
 	while(kid!= null):
 		joint_array.append(kid)
 		if(kid.get_child_count() > 0):
@@ -35,31 +41,31 @@ func _ready():
 #IK algorithm (FABRIK single pass)
 func single_solve(target):
 	#initial position of the first joint, used to make the limb not fly off if the target is further than the length of the arm
-	var init_start = test_array[0]
+	var init_start = global_position
 	#print(test_array.size())
 	test_array[test_array.size() - 1] = target
 	#FABRIK "backwards" part
 	for i in range (test_array.size() - 2, -1, -1):
-		var move_vector = 100 * calculate_normal(test_array[i+1], test_array[i])
+		var move_vector = joint_length * calculate_normal(test_array[i+1], test_array[i])
 		test_array[i] = test_array[i+1] + move_vector
 		#print(str(test_array[i]))
 	test_array[0] = init_start
 	#FABRIK "forwards" part
 	for i in range (1, test_array.size()):
-		var move_vector = 100 * calculate_normal(test_array[i-1], test_array[i])
+		var move_vector = joint_length * calculate_normal(test_array[i-1], test_array[i])
 		test_array[i] = test_array[i-1] + move_vector
 		#print(str(test_array[i]))
 	#Applying new positions to joints
 	for i in range (0, test_array.size()):
 		joint_array[i].global_position = test_array[i]
 		
+		
 			
 		
 	
 func _process(delta):
 	#Trigger FABRIK when the left mouse button is pressed/held
-	if Input.is_action_pressed("left_throw"):
-		mouseposition = get_global_mouse_position()
-		single_solve(mouseposition)
+	if is_active:
+		single_solve(target_ref.global_position)
 	pass
 	
