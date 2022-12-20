fece712c74e0c4b0ac755621c33fbddfd90629b4
PRLHA <01171903@pw.edu.pl>
2022-12-20 23:26:12 +0100

    IK (zintegrowan z resztą projektu)
    - Naprawione IK, teraz scena powinna działać
    - Zamieniłem input action z "click" na "left_throw" (docelowo będzie inny warunek)

--- IK/IK.gd
+++ IK/IK.gd
@@ -21,7 +21,10 @@ func _ready():
 	# loading joints into the array
 	while(kid!= null):
 		joint_array.append(kid)
-		kid = kid.get_child(0)
+		if(kid.get_child_count() > 0):
+			kid = kid.get_child(0)
+		else:
+			kid = null
 	#initialising the test_array with neutral positions
 	for i in 4:
 		test_array.append(Vector2(0,0))
@@ -33,19 +36,19 @@ func _ready():
 func single_solve(target):
 	#initial position of the first joint, used to make the limb not fly off if the target is further than the length of the arm
 	var init_start = test_array[0]
-	print(test_array.size())
+	#print(test_array.size())
 	test_array[test_array.size() - 1] = target
 	#FABRIK "backwards" part
 	for i in range (test_array.size() - 2, -1, -1):
 		var move_vector = 100 * calculate_normal(test_array[i+1], test_array[i])
 		test_array[i] = test_array[i+1] + move_vector
-		print(str(test_array[i]))
+		#print(str(test_array[i]))
 	test_array[0] = init_start
 	#FABRIK "forwards" part
 	for i in range (1, test_array.size()):
 		var move_vector = 100 * calculate_normal(test_array[i-1], test_array[i])
 		test_array[i] = test_array[i-1] + move_vector
-		print(str(test_array[i]))
+		#print(str(test_array[i]))
 	#Applying new positions to joints
 	for i in range (0, test_array.size()):
 		joint_array[i].global_position = test_array[i]
@@ -55,7 +58,7 @@ func single_solve(target):
 	
 func _process(delta):
 	#Trigger FABRIK when the left mouse button is pressed/held
-	if Input.is_action_pressed("click"):
+	if Input.is_action_pressed("left_throw"):
 		mouseposition = get_global_mouse_position()
 		single_solve(mouseposition)
 	pass
